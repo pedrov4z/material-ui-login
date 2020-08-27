139c0d73d9308c00fa751ec768d5df1a652869f1
pedrov4z <61953061+pedrov4z@users.noreply.github.com>
2020-08-27 11:20:24 -0300

    fix: Unnecessary escape characters

--- web/src/helpers/validators.ts
+++ web/src/helpers/validators.ts
@@ -5,19 +5,19 @@ export function validateName(name: string) {
 }
 
 export function validateCpf(cpf: string) {
-  const model = /^[0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2}$/
+  const model = /^[0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2}$/
   if(!(cpf.match(model))) return 'Deve conter 11 números'
   else return ''
 }
 
 export function validateCnpj(cnpj: string) {
-  const model = /^[0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[/]?[0-9]{4}[-]?[0-9]{2}$/
+  const model = /^[0-9]{2}[.]?[0-9]{3}[.]?[0-9]{3}[/]?[0-9]{4}[-]?[0-9]{2}$/
   if(!(cnpj.match(model))) return 'Deve conter 14 números'
   else return ''
 }
 
 export function validateEmail(email: string) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   const ok = re.test(String(email).toLowerCase())
   return ok ? '' : 'E-mail inválido'
 }
